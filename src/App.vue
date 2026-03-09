d1da37673fa010a7268a9333dcae53782f7f071e
olivabaeriswyl <122967424+olivabaeriswyl@users.noreply.github.com>
2026-03-09 13:49:15 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <h2>Yeah</h2>
+</template>
+
+<style scoped></style>
