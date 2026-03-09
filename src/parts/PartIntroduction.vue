d18fb9c5f1f598f000318cd08084c3b6e55dfcda
FlavioOnGitHub <flaviorotunno@gmail.com>
2026-03-09 17:02:07 +0100

    v1, the height of the image is decided by the vw

--- src/parts/PartIntroduction.vue
+++ src/parts/PartIntroduction.vue
@@ -2,6 +2,15 @@
 
 <template>
   <h1>Sapin porte-paroles</h1>
+  <img src="../../public/background-test.svg" alt="" />
 </template>
 
-<style scoped></style>
+<style scoped>
+img {
+  left: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  height: 55vw;
+  position: fixed;
+}
+</style>
