2a248854aa4678ec09db8d5e77047c4a90fd2c87
FlavioOnGitHub <flaviorotunno@gmail.com>
2026-03-10 16:31:43 +0100

    version with new three and simple but not very responsive for aspect ratios

--- src/parts/PartIntroduction.vue
+++ src/parts/PartIntroduction.vue
@@ -1,30 +1,34 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import gsap from 'gsap'
-
-const t2 = ref(null)
-const t3 = ref(null)
-
+import { ref, watch, onMounted } from 'vue'
+// import gsap from 'gsap'
+const heroImg = ref(null)
+const view = ref(3)
 onMounted(() => {
-  t2.value.addEventListener('load', () => {
-    const t2Width = t2.value.getBoundingClientRect().width
-    t3.value.style.transform = `translate(calc(100vw + ${t2Width}px), 0)`
-    console.log('t2 width:', t2Width)
-  })
+  watch(
+    view,
+    (newVal) => {
+      console.log('view changed:', newVal)
+      if (view.value === 1) {
+        heroImg.value.style.transform = 'translate(0, 0)'
+      } else if (view.value === 2) {
+        heroImg.value.style.transform = 'translate(-62%, -20%)'
+      } else if (view.value === 3) {
+        heroImg.value.style.transform = 'translate(-40%, -70%)'
+      }
+    },
+    { immediate: true },
+  )
 })
 </script>
 
 <template>
-  <!-- <img ref="heroImg" src="/sapin.svg" alt="" /> -->
-  <div class="container">
-    <img class="t1" src="/tile-1.svg" alt="" />
-    <img ref="t2" class="t2" src="/tile-2.svg" alt="" />
-    <img ref="t3" class="t3" src="/tile-3.svg" alt="" />
-    <img class="t4" src="/tile-4.svg" alt="" />
-    <img class="t5" src="/tile-5.svg" alt="" />
-    <img class="t6" src="/tile-6.svg" alt="" />
-  </div>
+  <img ref="heroImg" src="/sapin-carre.svg" alt="" />
   <div class="scroll"></div>
+  <div class="dev-nav">
+    <div :class="{ active: view === 1 }" @click="view = 1"></div>
+    <div :class="{ active: view === 2 }" @click="view = 2"></div>
+    <div :class="{ active: view === 3 }" @click="view = 3"></div>
+  </div>
 </template>
 
 <style scoped>
@@ -33,112 +37,42 @@ onMounted(() => {
   overflow: hidden;
 }
 
-/* img {
+.dev-nav {
   position: fixed;
-  transform: translate(-4%, -41%);
   left: 0;
-  width: 360vw;
-} */
-
-@keyframes slide {
-  0% {
-    transform: translate(0, 0);
-  }
-  100% {
-    transform: translate(-200vw, -100vw);
-  }
+  top: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 20px;
 }
 
-.container {
+.dev-nav div {
+  width: 20px;
+  height: 20px;
+  background: rgb(235, 197, 197);
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.dev-nav div.active {
+  background: rgb(180, 100, 100);
+}
+
+img {
   position: fixed;
-  inset: 0;
-  width: 100%;
-  height: 100%;
-  pointer-events: none;
-  animation: slide 6s 2s linear infinite;
+  left: 0;
+  top: 0;
+  width: 300vw;
+  transition: transform 1s ease;
 }
 
-.t1 {
-  transform: translate(0%, 0);
-  object-fit: cover;
-  object-position: right;
-  position: absolute;
-  inset: 0;
-  width: 100%;
-  height: 100%;
-  aspect-ratio: 16 / 9; /* ou ton ratio */
-}
-
-.t2 {
-  object-position: center;
-  position: absolute;
-  inset: 0;
-  left: 100vw;
-  width: auto;
-  height: 100%;
-}
-
-.t3 {
-  object-fit: cover;
-  object-position: left;
-  position: absolute;
-  inset: 0;
-  width: 100%;
-  height: 100%;
-  aspect-ratio: 16 / 9; /* ou ton ratio */
-}
-
-@media (min-aspect-ratio: 14.8/7.8) {
-  .t2 {
-    transform: translate(100%, 0);
-    object-fit: cover;
-    object-position: center;
-    position: absolute;
-    inset: 0;
-    width: 100%;
-    height: 100%;
-    aspect-ratio: 16 / 9; /* ou ton ratio */
+/* @media (max-aspect-ratio: 16/10) {
+  img {
+    width: auto;
+    position: fixed;
+    left: 0;
+    top: 0;
   }
-
-  .t3 {
-    transform: translate(200%, 0);
-    object-fit: cover;
-    object-position: center;
-    position: absolute;
-    inset: 0;
-    width: 100%;
-    height: 100%;
-    aspect-ratio: 16 / 9; /* ou ton ratio */
-  }
-}
-.t4 {
-  transform: translate(0, 100%);
-  object-fit: cover;
-  object-position: right;
-  position: absolute;
-  inset: 0;
-  width: 100%;
-  height: 100%;
-  aspect-ratio: 16 / 9; /* ou ton ratio */
-}
-.t5 {
-  transform: translate(100%, -100%);
-  object-fit: cover;
-  object-position: center;
-  position: absolute;
-  inset: 0;
-  width: 100%;
-  height: 100%;
-  aspect-ratio: 16 / 9; /* ou ton ratio */
-}
-.t6 {
-  transform: translate(200%, -100%);
-  object-fit: cover;
-  object-position: center;
-  position: absolute;
-  inset: 0;
-  width: 100%;
-  height: 100%;
-  aspect-ratio: 16 / 9; /* ou ton ratio */
-}
+} */
 </style>
