a3fb5601f8a5a0cc5c64f7b728c5c440f07846c9
olivabaeriswyl <122967424+olivabaeriswyl@users.noreply.github.com>
2026-03-10 14:16:37 +0100

    Instalation bootstrap

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import gsap from 'gsap'
+
 createApp(App).mount('#app')
