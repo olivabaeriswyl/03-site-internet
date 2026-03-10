bc0ee6a00fa44519491698cd1bab8c444967e68d
FlavioOnGitHub <flaviorotunno@gmail.com>
2026-03-10 10:11:50 +0100

    added gsap

--- src/parts/PartIntroduction.vue
+++ src/parts/PartIntroduction.vue
@@ -1,9 +1,28 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import gsap from 'gsap'
+import { CustomEase } from 'gsap/CustomEase'
+
+const heroImg = ref(null)
+let moved = false
+
+const onCtaClick = () => {
+  // moved = !moved
+  gsap.to(heroImg.value, {
+    xPercent: moved ? -31 : 0,
+    duration: 2,
+    ease: CustomEase.create(
+      'custom',
+      'M0,0 C0.307,0.062 0.484,0.106 0.568,0.36 0.688,0.726 0.818,1.001 1,1 ',
+    ),
+  })
+}
+</script>
 
 <template>
   <h1>Sapin porte-paroles</h1>
-  <a href="#">Débuter l’expérience</a>
-  <img src="/background-test-nocrop.svg" alt="" />
+  <a href="#" @click="onCtaClick">Débuter l’expérience</a>
+  <img ref="heroImg" src="/background-test-nocrop.svg" alt="" />
 </template>
 
 <style scoped>
