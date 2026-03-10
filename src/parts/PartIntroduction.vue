60a1d02ef1faad57be569a5089ecfa20c813bea4
FlavioOnGitHub <flaviorotunno@gmail.com>
2026-03-10 15:24:06 +0100

    tile version working

--- src/parts/PartIntroduction.vue
+++ src/parts/PartIntroduction.vue
@@ -1,14 +1,25 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import gsap from 'gsap'
+
+const t2 = ref(null)
+const t3 = ref(null)
+
+onMounted(() => {
+  t2.value.addEventListener('load', () => {
+    const t2Width = t2.value.getBoundingClientRect().width
+    t3.value.style.transform = `translate(calc(100vw + ${t2Width}px), 0)`
+    console.log('t2 width:', t2Width)
+  })
+})
 </script>
 
 <template>
   <!-- <img ref="heroImg" src="/sapin.svg" alt="" /> -->
   <div class="container">
     <img class="t1" src="/tile-1.svg" alt="" />
-    <img class="t2" src="/tile-2.svg" alt="" />
-    <img class="t3" src="/tile-3.svg" alt="" />
+    <img ref="t2" class="t2" src="/tile-2.svg" alt="" />
+    <img ref="t3" class="t3" src="/tile-3.svg" alt="" />
     <img class="t4" src="/tile-4.svg" alt="" />
     <img class="t5" src="/tile-5.svg" alt="" />
     <img class="t6" src="/tile-6.svg" alt="" />
@@ -29,59 +40,76 @@ import gsap from 'gsap'
   width: 360vw;
 } */
 
+@keyframes slide {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-200vw);
+  }
+}
+
 .container {
-  position: inherit;
-}
-
-@keyframes t1 {
-  0% {
-    transform: translate(0, 0);
-  }
-  100% {
-    transform: translate(-100%, 0);
-  }
-}
-
-@keyframes t2 {
-  0% {
-    transform: translate(100%, 0);
-  }
-  100% {
-    transform: translate(0, 0);
-  }
+  position: fixed;
+  inset: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+  animation: slide 6s linear infinite;
 }
 
 .t1 {
+  transform: translate(0%, 0);
   object-fit: cover;
-  object-position: center;
+  object-position: right;
   position: absolute;
   inset: 0;
   width: 100%;
   height: 100%;
   aspect-ratio: 16 / 9; /* ou ton ratio */
-  /* animation: t1 1s ease-out forwards; */
 }
 
 .t2 {
-  transform: translate(100%, 0);
-  object-fit: cover;
   object-position: center;
   position: absolute;
   inset: 0;
-  width: 100%;
+  left: 100vw;
+  width: auto;
   height: 100%;
-  aspect-ratio: 16 / 9; /* ou ton ratio */
-  /* animation: t2 1s ease-out forwards; */
 }
+
 .t3 {
-  transform: translate(200%, 0);
   object-fit: cover;
-  object-position: center;
+  object-position: left;
   position: absolute;
   inset: 0;
   width: 100%;
   height: 100%;
   aspect-ratio: 16 / 9; /* ou ton ratio */
+}
+
+@media (min-aspect-ratio: 14.8/7.8) {
+  .t2 {
+    transform: translate(100%, 0);
+    object-fit: cover;
+    object-position: center;
+    position: absolute;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    aspect-ratio: 16 / 9; /* ou ton ratio */
+  }
+
+  .t3 {
+    transform: translate(200%, 0);
+    object-fit: cover;
+    object-position: center;
+    position: absolute;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    aspect-ratio: 16 / 9; /* ou ton ratio */
+  }
 }
 .t4 {
   transform: translate(0, -100%);
