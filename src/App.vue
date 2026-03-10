75f9a893b9515e8ef89304c533b3f53d69ac5096
FlavioOnGitHub <flaviorotunno@gmail.com>
2026-03-10 09:11:13 +0100

    more responsive bg

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup>
-import PartOutro from './parts/PartOutro.vue'
 import PartIntro from './parts/PartIntroduction.vue'
-import PartStats from './parts/PartStats.vue'
 </script>
 
 <template>
   <PartIntro></PartIntro>
-  <PartStats></PartStats>
-  <PartOutro></PartOutro>
 </template>
 
 <style scoped></style>
--- src/parts/PartIntroduction.vue
+++ src/parts/PartIntroduction.vue
@@ -2,15 +2,23 @@
 
 <template>
   <h1>Sapin porte-paroles</h1>
-  <img src="../../public/background-test.svg" alt="" />
+  <button>Débuter l’expérience</button>
+  <img src="/background-test-nocrop.svg" alt="" />
 </template>
 
 <style scoped>
 img {
   left: 0;
   top: 50%;
-  transform: translateY(-50%);
-  height: 100%;
+  transform: translate(-0.5%, -50%);
+  height: 95vw;
   position: fixed;
+}
+
+button {
+  position: absolute;
+  left: 50%;
+  bottom: 30%;
+  transform: translate(-50%, 0);
 }
 </style>
