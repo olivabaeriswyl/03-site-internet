679f8c12a89c30988dd5a79e20a8eb019f11363e
FlavioOnGitHub <flaviorotunno@gmail.com>
2026-03-13 09:16:27 +0100

    scemantic upgrade - iris

--- src/parts/PartIntroduction.vue
+++ src/parts/PartIntroduction.vue
@@ -1,152 +1,109 @@
 <script setup>
-import { ref, watch, onMounted, onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
 
 const heroImg = ref(null)
-const view = ref(1)
-const origins = {
-  desktop: {
-    1: { x: 10, y: 45 },
-    2: { x: 72, y: 58 },
-    3: { x: 54, y: 88 },
-    0: { x: 50, y: 50 },
-  },
-  mobile: {
-    1: { x: 2, y: 48.5 },
-    2: { x: 72, y: 58.5 },
-    3: { x: 46, y: 89 },
-    0: { x: 50, y: 50 },
-  },
-}
-
-let targetScroll = 0
-let currentScroll = 0
-let rafId = 0
-
-const MIN = 0
-const MAX = 4000 // "longueur" du fake scroll
-const SMOOTH = 0.08 // lissage
-const SPEED = 1 // sensibilité molette
-const PARALLAX = 0.35 // facteur de déplacement X
-
-const clamp = (v, min, max) => Math.max(min, Math.min(max, v))
-
-const onWheel = (e) => {
-  e.preventDefault()
-  targetScroll = clamp(targetScroll + e.deltaY * SPEED, MIN, MAX)
-}
-
-const tick = () => {
-  currentScroll += (targetScroll - currentScroll) * SMOOTH
-  const x = -currentScroll * PARALLAX
-
-  if (heroImg.value) {
-    heroImg.value.style.setProperty('--bg-x', `${x}px`)
-  }
-
-  rafId = requestAnimationFrame(tick)
-}
+const heroContainer = ref(null)
 
 onMounted(() => {
-  window.addEventListener('wheel', onWheel, { passive: false })
-  rafId = requestAnimationFrame(tick)
+  if (heroImg?.value && heroContainer?.value) {
+    console.log('ok')
+    gsap.to(heroImg.value, {
+      // transformOrigin: '10% 45%',
+      // x: '-200%',
+      // y: '-100%',
+      // duration: 10,
 
-  watch(
-    view,
-    (newVal) => {
-      console.log('view changed:', newVal)
-      if (window.innerWidth > 600) {
-        if (view.value === 1) {
-          heroImg.value.style.transformOrigin = '10% 45%'
-        } else if (view.value === 2) {
-          heroImg.value.style.transformOrigin = '72% 58%'
-        } else if (view.value === 3) {
-          heroImg.value.style.transformOrigin = '54% 88%'
-        } else if (view.value === 0) {
-          heroImg.value.style.transformOrigin = 'center center'
-          heroImg.value.style.scale = '50%'
-        }
-      } else {
-        if (view.value === 1) {
-          heroImg.value.style.transformOrigin = '2% 48.5%'
-        } else if (view.value === 2) {
-          heroImg.value.style.transformOrigin = '72% 58.5%'
-        } else if (view.value === 3) {
-          heroImg.value.style.transformOrigin = '46% 89%'
-        } else if (view.value === 0) {
-          heroImg.value.style.transformOrigin = 'center center'
-          heroImg.value.style.scale = '50%'
-        }
-      }
-    },
-    { immediate: true },
-  )
+      scrollTrigger: {
+        trigger: heroContainer.value,
+        start: 'top top',
+        // end: 'bottom top',
+        scrub: true,
+      },
+    })
+  }
 })
+// const view = ref(1)
+// const origins = {
+//   desktop: [
+//     { x: 10, y: 45 },
+//     { x: 72, y: 58 },
+//     { x: 54, y: 88 },
+//     { x: 50, y: 50 },
+//   ],
+//   mobile: [
+//     { x: 2, y: 48.5 },
+//     { x: 72, y: 58.5 },
+//     { x: 46, y: 89 },
+//     { x: 50, y: 50 },
+//   ],
+// }
 
-onUnmounted(() => {
-  window.removeEventListener('wheel', onWheel)
-  cancelAnimationFrame(rafId)
-})
+// onMounted(() => {
+//   heroImg.value.style.transformOrigin = '10% 45%'
+//   window.addEventListener('wheel', onWheel, { passive: false })
+//   rafId = requestAnimationFrame(tick)
+// })
+
+// onUnmounted(() => {
+//   window.removeEventListener('wheel', onWheel)
+//   cancelAnimationFrame(rafId)
+// })
 </script>
 
 <template>
-  <div class="heroContainer">
-    <img ref="heroImg" src="/sapin.svg" alt="" />
+  <div ref="heroContainer" class="heroContainer">
+    <img ref="heroImg" class="heroImg" src="/sapin.svg" alt="" />
   </div>
   <div class="scroll"></div>
-  <div class="dev-nav">
-    <div :class="{ active: view === 1 }" @click="view = 1"></div>
-    <div :class="{ active: view === 2 }" @click="view = 2"></div>
-    <div :class="{ active: view === 3 }" @click="view = 3"></div>
-  </div>
 </template>
 
 <style scoped>
-:global(html),
+/* .heroContainer {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+  background-color: aqua;
+}
+
+.heroImg {
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  width: 1000px;
+  scale: 500%;
+  height: auto;
+} */
+
+/* :global(html),
 :global(body) {
   overflow: hidden;
-}
+}*/
 
-.scroll {
+/* .scroll {
   height: 200vh;
-}
-
-.dev-nav {
-  position: fixed;
-  left: 0;
-  top: 0;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  padding: 20px;
-}
-
-.dev-nav div {
-  width: 20px;
-  height: 20px;
-  background: rgb(235, 197, 197);
-  border-radius: 50%;
-  cursor: pointer;
-}
-
-.dev-nav div.active {
-  background: rgb(180, 100, 100);
-}
+} */
 
 .heroContainer {
-  position: fixed;
-  width: 100%;
-  height: 100%;
+  position: relative;
+  overflow: hidden;
+  width: 100vw;
+  height: 300vh;
   align-items: center;
   justify-content: center;
   display: flex;
 }
 
 img {
-  width: 1000px;
+  width: 80vw;
   scale: 450%;
-  height: auto;
-  transition: all 3s ease;
+  position: absolute;
+  top: 0;
+  left: 0;
+  /* transition: all 3s ease; */
 }
 
 @media (min-width: 600px) {
