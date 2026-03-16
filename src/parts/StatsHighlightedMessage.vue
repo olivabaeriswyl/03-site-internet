352081a961ddfa3ea03280c7dcc649e023a41faf
olivabaeriswyl <122967424+olivabaeriswyl@users.noreply.github.com>
2026-03-16 16:53:28 +0100

    Layout and style Highlighted message

--- src/parts/StatsHighlightedMessage.vue
+++ src/parts/StatsHighlightedMessage.vue
@@ -1,15 +1,24 @@
 <script setup></script>
 
 <template>
-  <h3>Message mis en avant</h3>
-  <div id="layout-message">
-    <p class="informations">Date</p>
-    <p class="quote">Citation</p>
-    <p class="informations">Name</p>
+  <div class="screen">
+    <h3>Message mis en avant</h3>
+    <div class="row">
+      <div class="col-12">
+        <div id="layout-message">
+          <div id="quote-block">
+            <p class="informations" id="date">Date</p>
+            <p id="quote">Tu es forte et tu mérites d’être respectée tel que tu es ♡</p>
+            <p class="informations" id="name">Name</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+/* Style texte informations  */
 .informations {
   font-family: arima, sans-serif;
   font-size: 23px;
@@ -17,7 +26,26 @@
   line-height: 30px;
 }
 
+/* Mise en page  */
 #layout-message {
   display: flex;
+  justify-content: center;
+}
+
+#quote-block {
+  max-width: 637px;
+}
+
+#quote {
+  margin: 20px 0;
+}
+
+/* Position informations  */
+#date {
+  text-align: left;
+}
+
+#name {
+  text-align: right;
 }
 </style>
