1103ef7ca4321038117111f0c96ae686cb2f6beb
olivabaeriswyl <122967424+olivabaeriswyl@users.noreply.github.com>
2026-03-20 17:09:15 +0100

    JS messages base de donnée et filtres actifs

--- src/parts/StatsAllMessages.vue
+++ src/parts/StatsAllMessages.vue
@@ -1,16 +1,38 @@
 <script setup>
-import { onMounted } from 'vue'
+import { ref, computed, onMounted, watch } from 'vue'
 import gsap from 'gsap'
-import allMessages from '@/data/messages-sapin-porte-paroles.json'
+import messages from '@/data/messages-sapin-porte-paroles.json'
+
+// Messages et filtres
+const activeCategory = ref('category-support') // catégorie par défaut
+
+const filteredMessages = computed(() => {
+  return messages.filter((m) => m.category === activeCategory.value)
+})
+
+const displayedMessages = ref([])
+
+function updateDisplayedMessages() {
+  const shuffled = [...filteredMessages.value].sort(() => 0.5 - Math.random())
+
+  displayedMessages.value = shuffled.slice(0, 5)
+}
+
+watch(activeCategory, () => {
+  updateDisplayedMessages()
+})
 
 // Animation textes
+
 onMounted(() => {
+  updateDisplayedMessages()
+
   const texts = [
-    { id: '#text-top', duration: 22 },
-    { id: '#text-middle-left', duration: 22 },
-    { id: '#text-middle-right', duration: 22 },
-    { id: '#text-bottom-left', duration: 22 },
-    { id: '#text-bottom-right', duration: 22 },
+    { id: '#text-top', duration: 30 },
+    { id: '#text-middle-left', duration: 30 },
+    { id: '#text-middle-right', duration: 30 },
+    { id: '#text-bottom-left', duration: 30 },
+    { id: '#text-bottom-right', duration: 30 },
   ]
 
   texts.forEach((t) => {
@@ -26,6 +48,10 @@ onMounted(() => {
     )
   })
 })
+
+function setFilter(category) {
+  activeCategory.value = category
+}
 </script>
 
 <template>
@@ -36,11 +62,36 @@ onMounted(() => {
       </div>
       <div id="filters-layout">
         <div id="filters">
-          <button @click="setFilter('support')">Soutien émotionnel</button>
-          <button @click="setFilter('action')">Appel à l'action</button>
-          <button @click="setFilter('solidarity')">Solidarité collective</button>
-          <button @click="setFilter('condemnation')">Condamnation de la violence</button>
-          <button @click="setFilter('tributes')">Hommages aux victimes</button>
+          <button
+            :class="{ active: activeCategory === 'category-support' }"
+            @click="setFilter('category-support')"
+          >
+            Soutien émotionnel
+          </button>
+          <button
+            :class="{ active: activeCategory === 'category-action' }"
+            @click="setFilter('category-action')"
+          >
+            Appel à l'action
+          </button>
+          <button
+            :class="{ active: activeCategory === 'category-solidarity' }"
+            @click="setFilter('category-solidarity')"
+          >
+            Solidarité collective
+          </button>
+          <button
+            :class="{ active: activeCategory === 'category-condemnation' }"
+            @click="setFilter('category-condemnation')"
+          >
+            Condamnation de la violence
+          </button>
+          <button
+            :class="{ active: activeCategory === 'category-tributes' }"
+            @click="setFilter('category-tributes')"
+          >
+            Hommages aux victimes
+          </button>
         </div>
       </div>
       <div></div>
@@ -57,17 +108,17 @@ onMounted(() => {
       >
         <path
           d="M293.956 123.479C344.456 124.227 320.456 71.979 356.956 64.479C388.114 58.0767 399.955 85.9788 396.956 108.479C394.556 126.479 377.456 168.479 350.956 168.479C294.323 168.479 300.896 210.751 257.456 200.979C194.004 186.705 164.112 229.348 129.456 192.479C105.956 167.479 101.141 177.799 52.456 163.479C14.8907 152.429 7.95607 126.479 13.956 105.479C17.8021 92.0175 43.456 82.9789 62.4561 90.4789C88.456 100.742 87.8356 114.776 109.456 126.479C163.956 155.979 172.671 127.655 202.456 136.979C251.936 152.468 254.88 122.9 293.956 123.479Z"
-          fill="red"
+          fill="#F9FCFF"
         />
         <path
           id="curve-text-top"
           d="M40.02 124C163.52 204 303.02 212.5 373.02 91.5"
-          stroke="black"
+          stroke="none"
         />
         <g class="text-mask">
           <text>
             <textPath id="text-top" href="#curve-text-top" startOffset="0%">
-              Tu es forte et courageuse
+              {{ displayedMessages[0]?.message }}
             </textPath>
           </text>
         </g>
@@ -84,16 +135,16 @@ onMounted(() => {
       >
         <path
           d="M352.411 282.641C381.882 303.633 415.574 289.904 434.426 296.626C453.279 303.349 463.779 326.349 455.779 345.849C450.864 357.827 423.955 384.661 381.779 354.349C339.603 324.036 316.99 369.778 275.025 342.707C227.435 312.007 217.203 348.509 187.733 315.131C144.687 266.377 108.172 275.214 89.2788 242.849C69.6088 209.153 37.2786 199.349 29.2788 167.349C21.6127 136.683 28.2788 118.849 43.7788 110.849C61.5512 101.676 78.6765 103.939 89.2788 125.849C107.326 163.142 112.435 154.141 127.779 173.349C161.6 215.685 171.61 197.838 190.279 222.849C221.293 264.399 225.379 241.714 256.701 265.085C301.041 298.168 322.939 261.648 352.411 282.641Z"
-          fill="red"
+          fill="#F9FCFF"
         />
         <path
           id="curve-text-middle-left"
           d="M47.8428 143.37C115.843 264.37 202.343 316.87 432.843 338.869"
-          stroke="black"
+          stroke="none"
         />
         <text>
           <textPath id="text-middle-left" href="#curve-text-middle-left" startOffset="0%">
-            Tu es forte et courageuse
+            {{ displayedMessages[1]?.message }}
           </textPath>
         </text>
       </svg>
@@ -109,16 +160,16 @@ onMounted(() => {
       >
         <path
           d="M292.705 92.6172C344.289 87.3878 336.818 132.236 375.705 136.117C426.134 141.15 433.205 206.919 481.205 164.117C498.456 148.734 495.9 105.966 446.366 99.6202C391.705 92.6173 376.776 33.6172 317.705 37.1172C252.782 40.964 242.326 4.16387 205.703 12.1173C167.575 20.3975 163.987 -10.3086 116.704 8.11726C79.7383 22.5223 61.8879 3.82061 32.204 10.2123C-13.7965 20.1173 -4.36183 88.4418 39.7035 69.1173C86.4494 48.6173 97.6314 77.6913 126.203 69.1173C165.886 57.2092 162.676 73.2924 187.203 71.6173C241.265 67.925 261.655 95.7651 292.705 92.6172Z"
-          fill="red"
+          fill="#F9FCFF"
         />
         <path
           id="curve-text-middle-right"
           d="M29.269 52.1381C190.769 16.6382 347.269 71.6382 454.269 156.899"
-          stroke="black"
+          stroke="none"
         />
         <text>
           <textPath id="text-middle-right" href="#curve-text-middle-right" startOffset="0%">
-            Tu es forte et courageuse
+            {{ displayedMessages[2]?.message }}
           </textPath>
         </text>
       </svg>
@@ -134,16 +185,16 @@ onMounted(() => {
       >
         <path
           d="M185.384 205.53C149.225 206.866 130.908 214.702 102.172 241.944C73.4361 269.185 4.43574 278.185 4.43609 235.685C4.43641 198.185 41.6862 175.374 80.9361 174.09C120.186 172.807 132.436 130.359 211.96 123.748C268.398 119.055 301 77.0777 344.71 85.5583C408.557 97.9456 416.623 65.97 451.334 80.0984C487.472 94.8074 482.232 68.7588 531.32 81.6299C569.196 91.5614 585.868 80.7536 613.094 94.1977C646.455 110.671 653.257 124.028 647.226 139.859C641.97 153.655 623.779 162.992 602.504 151.005C551.05 122.015 555.17 150.099 530.247 137.62C493.202 119.07 480.973 149.463 458.184 140.24C401.116 117.144 394.314 158.982 359.782 149.287C309.864 135.271 312.895 173.511 273.819 174.09C218.503 174.911 221.543 204.194 185.384 205.53Z"
-          fill="red"
+          fill="#F9FCFF"
         />
         <path
           id="curve-text-bottom-left"
           d="M49 235.206C160.833 172.373 408.5 75.2061 615.5 132.206"
-          stroke="black"
+          stroke="none"
         />
         <text>
           <textPath id="text-bottom-left" href="#curve-text-bottom-left" startOffset="0%">
-            Tu es forte et courageuse
+            {{ displayedMessages[3]?.message }}
           </textPath>
         </text>
       </svg>
@@ -159,16 +210,16 @@ onMounted(() => {
       >
         <path
           d="M136 190.514C100.5 197.514 94 217.014 69.5008 240.514C45.0016 264.014 -0.000257015 260.514 0 220.514C8.31923e-05 207.566 18.5008 172.845 69.5008 163.014C120.501 153.182 97.0007 104.918 145.501 93.0137C200.501 79.5137 181.001 47.0051 225.501 48.5137C290.501 50.7172 308.501 9.51369 345.001 18.0137C383.001 26.863 390.501 3.01368 441.001 8.01361C479.967 11.8716 504.5 -6.48632 533.5 2.51373C600 23.1518 591 96.5136 535.5 69.0137C480 41.5137 474.441 69.0137 446.001 60.0135C406.501 47.5135 396.999 71.0553 372.5 69.0137C318.5 64.5137 326.5 98.6253 295.5 95.0137C244 89.0137 253 126.302 214.501 133.013C160 142.514 171.5 183.514 136 190.514Z"
-          fill="red"
+          fill="#F9FCFF"
         />
         <path
           id="curve-text-bottom-right"
           d="M43.564 233.535C135.564 147.035 337.564 -12.4653 562.564 55.0341"
-          stroke="black"
+          stroke="none"
         />
         <text>
           <textPath id="text-bottom-right" href="#curve-text-bottom-right" startOffset="0%">
-            Tu es forte et courageuse
+            {{ displayedMessages[4]?.message }}
           </textPath>
         </text>
       </svg>
@@ -211,6 +262,13 @@ button {
 button:hover {
   color: var(--color-orange);
   border: 1.4px solid var(--color-orange);
+  transform: scale(1.04);
+}
+
+button.active {
+  color: var(--color-white);
+  border: 1.4px solid var(--color-orange);
+  background-color: var(--color-orange);
   transform: scale(1.04);
 }
 
@@ -260,7 +318,7 @@ text {
   font-family: epilogue, sans-serif;
   font-size: 22px;
   font-weight: 400;
-  fill: rgb(255, 255, 0);
+  fill: var(--color-blue);
 }
 
 /* Position courbes */
